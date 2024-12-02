209664668cef76e61b07ab62baf69b38c6e76c24
emily <epryer@student.fitchburgstate.edu>
2024-12-02 14:37:29 -0500

    DOne

--- Exploding Dice.jl
+++ Exploding Dice.jl
@@ -1,6 +1,21 @@
 module Exploding_Dice
 import Random
+export explode
 
+function explode(max::Int)
+    local sum=0
+    if max<0
+        throw(ArgumentError("This number on a n-sided die has to be greater than 0"))
+    end
+    check = max
+    roll = rand(1:max)
+    while roll == check
+        sum += roll 
+        roll = rand(1:max)
+    end
+    sum += roll
+    return sum
+end
 
 
 
