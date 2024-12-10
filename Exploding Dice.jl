ba49cb01a52d7e89a7d72030b703ec0c2d37e464
emily <epryer@student.fitchburgstate.edu>
2024-12-10 12:14:43 -0500

    ED.jl
    expectations, difference in expectations, at least probability.

--- Exploding Dice.jl
+++ Exploding Dice.jl
@@ -33,6 +33,46 @@ function rolltrials(f::Function, trials::Int, max::Int)
     return results
 end #Tyrian's code
 
+"""
+reg_die_expect(max::Int)
+Input max number of sides on single die. Returns the expected value you would get if die is rolled multiple times.
+"""
+function reg_die_expect(max::Int) #expectation of a regular n-sided die
+    (max+1)/2
+end
+
+"""
+explode_expectation(max::Int)
+Input max number of sides on single die. Returns expected value when rolling a die multiple times, dealing with explosions.
+"""
+function explode_expectation(max::Int) #expectation of exploding die 
+    if max<0 
+        throw(ArgumentError("This number on a n-sided die has to be greater than 0"))
+    end
+    exp = (max/(max-1))*((max+1)/2)
+    return exp
+end
+
+"""
+difference(ex::Real,reg::Real)
+Input expected value for explosions and regular die. Returns difference.
+"""
+function difference(ex::Real,reg::Real)
+    return ex - reg
+end
+
+"""
+atleast_explode(max::Int,n::Int)
+Input max number of sides on single die and n number of explosions. Returns probability of getting at least n explosions.
+"""
+
+function atleast_explode(max::Int,n::Int) #need to fix!
+    if max<0
+        throw(ArgumentError("This number on a n-sided die has to be greater than 0"))
+    end
+    k_explosions =@. 1-((max-1)/max)^n
+    return 100*k_explosions
+end 
 
 
 
